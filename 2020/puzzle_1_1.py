03f4d06fa1822027a425fe1fa8660645c3d5e12d
Valère Monseur <valere.monseur@ymail.com>
2020-12-01 19:33:56 +0100

    rework solution

--- 2020/puzzle_1_1.py
+++ 2020/puzzle_1_1.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
+from itertools import combinations
+from math import prod
+
 with open('puzzle_1_1.txt') as file:
     numbers = [int(line) for line in file]
 
-for n1 in numbers:
-    for n2 in numbers:
-        if n1+n2 == 2020:
-            print(f'n1: {n1}, n2: {n2}, n1*n2: {n1*n2}')
+def original_solution():
+    # super ugly, of course
+    for n1 in numbers:
+        for n2 in numbers:
+            if n1+n2 == 2020:
+                print(f'n1: {n1}, n2: {n2}, n1*n2: {n1*n2}')
+
+def reworked_solution(r):
+    return prod([n for n in combinations(numbers, r) if sum(n) == 2020][0])
+
+print("original solution:")
+original_solution()
+
+print("reworked solution:")
+print(reworked_solution(2))
--- 2020/puzzle_1_2.py
+++ 2020/puzzle_1_2.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
+from itertools import combinations
+from math import prod
+
 with open('puzzle_1_2.txt') as file:
     numbers = [int(line) for line in file]
 
-for n1 in numbers:
-    for n2 in numbers:
-        for n3 in numbers:
-            if n1+n2+n3 == 2020:
-                print(f'n1: {n1}, n2: {n2}, n3: {n3}, n1*n2*n3: {n1*n2*n3}')
+def original_solution():
+    # super ugly, of course
+    for n1 in numbers:
+        for n2 in numbers:
+            for n3 in numbers:
+                if n1+n2+n3 == 2020:
+                    print(f'n1: {n1}, n2: {n2}, n3: {n3}, n1*n2*n3: {n1*n2*n3}')
+
+def reworked_solution(r):
+    return prod([n for n in combinations(numbers, r) if sum(n) == 2020][0])
+
+print("original solution:")
+original_solution()
+
+print("reworked solution:")
+print(reworked_solution(3))
