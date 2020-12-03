cc45da6d9c0edce89b7c92122afbf7b82919dc11
Valère Monseur <valere.monseur@ymail.com>
2020-12-03 08:36:37 +0100

    day 3

---
+++ 2020/puzzle_3_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+map = []
+
+def build_map(filename, repetition):
+    global map
+    with open(filename) as file:
+        map = [line.strip() * repetition for line in file]
+
+def traverse_map(x, y):
+    global map
+
+    posx, count = 0, 0
+    for posy in range(0, len(map), y):
+        if map[posy][posx] == '#':
+            count += 1
+
+        posx += x
+        if posx >= len(map[0]):
+            print('map is not large enough')
+            return -1
+
+    return count
+
+build_map('puzzle_3_1.txt', 100)
+trees = traverse_map(3, 1)
+
+print(f'number of trees: {trees}')
---
+++ 2020/puzzle_3_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from math import prod
+
+map = []
+
+def build_map(filename, repetition):
+    global map
+    with open(filename) as file:
+        map = [line.strip() * repetition for line in file]
+
+def traverse_map(x, y):
+    global map
+
+    posx, count = 0, 0
+    for posy in range(0, len(map), y):
+        if map[posy][posx] == '#':
+            count += 1
+
+        posx += x
+        if posx >= len(map[0]):
+            print('map is not large enough')
+            return -1
+
+    return count
+
+build_map('puzzle_3_2.txt', 100)
+trees = [traverse_map(x, y) for x, y in ((1,1),(3,1),(5,1),(7,1),(1,2))]
+
+print(f'number of trees: {prod(trees)}')
