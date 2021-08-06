63e429617f2c102384d695819b18af34cd021635
Valère Monseur <dev@vale.re>
2021-08-06 23:11:13 +0200

    revert tabstop to 2

--- 2018/puzzle1b.bash
+++ 2018/puzzle1b.bash
@@ -8,12 +8,12 @@ result=1000000
 array[$result]="present"
 
 while true; do
-    while read line; do
-        result=$(( result + line ))
-        if [ "${array[$result]}" = "present" ]; then
-            echo $(( result - 1000000 ))
-            exit 0
-        fi
-        array[$result]="present"
-    done < "puzzle1b.txt"
+  while read line; do
+    result=$(( result + line ))
+    if [ "${array[$result]}" = "present" ]; then
+      echo $(( result - 1000000 ))
+      exit 0
+    fi
+    array[$result]="present"
+  done < "puzzle1b.txt"
 done
