86898934977160d1e3a43db1071d126543fdb6a1
Valère Monseur <dev@vale.re>
2023-12-01 12:00:55 +0100

    aoc 2023 day 1

---
+++ 2023/puzzle_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def is_letter_digit(s):
+    digits = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+
+    for i in range(0, len(digits)):
+        if s.startswith(digits[i]):
+            return i
+
+
+with open("puzzle_1.txt") as file:
+    lines = file.read().splitlines()
+
+calibration_value = 0
+for line in lines:
+    first_digit, last_digit = None, None
+    for i in range(0, len(line)):
+        digit = int(line[i]) if line[i].isdigit() else is_letter_digit(line[i:])
+        if digit:
+            last_digit = digit
+            if not first_digit:
+                first_digit = digit
+    calibration_value += first_digit * 10 + last_digit
+print(calibration_value)
