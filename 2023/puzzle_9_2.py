cbcb5a9680720cc4992da546be5931223b5bbda4
Valère Monseur <dev@vale.re>
2023-12-15 21:55:07 +0100

    aoc 2023 day 9

---
+++ 2023/puzzle_9_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+with open("puzzle_9.txt") as file:
+    hists = [list(map(int, line.split())) for line in file.read().splitlines()]
+
+sum_extrapolation = 0
+for hist in hists:
+    diffs = [hist]
+    while any(diffs[-1]):
+        diffs.append(
+            [
+                diffs[-1][diff_ix + 1] - diffs[-1][diff_ix]
+                for diff_ix in range(len(diffs[-1]) - 1)
+            ]
+        )
+
+    extrapolation = 0
+    for diff_ix in range(len(diffs) - 1, -1, -1):
+        extrapolation = diffs[diff_ix][0] - extrapolation
+    sum_extrapolation += extrapolation
+
+print(f"sum of extrapolated values: {sum_extrapolation}")
