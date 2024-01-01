ffd53f4b13e049cdbef60121f087ac9224c4c28f
Valère Monseur <dev@vale.re>
2024-01-01 22:54:40 +0100

    adapt code style

--- 2023/puzzle_1.js
+++ 2023/puzzle_1.js
@@ -16,19 +16,21 @@ const numbers =
 let sum_part2 = 0;
 
 data.split("\n").forEach((line) => {
-  const number = numbers.map((num) => {
-    return [line.indexOf(num), line.lastIndexOf(num)];
-  }).reduce((res, num, num_ix) => {
-    if (num[0] >= 0 && num[0] < res.min_val) {
-      res.min_val = num[0];
-      res.min_ix = num_ix;
-    }
-    if (num[1] >= 0 && num[1] > res.max_val) {
-      res.max_val = num[1];
-      res.max_ix = num_ix;
-    }
-    return res;
-  }, { min_val: Infinity, min_ix: -1, max_val: -Infinity, max_ix: -1 });
+  const number = numbers
+    .map((num) => {
+      return [line.indexOf(num), line.lastIndexOf(num)];
+    })
+    .reduce((res, num, num_ix) => {
+      if (num[0] >= 0 && num[0] < res.min_val) {
+        res.min_val = num[0];
+        res.min_ix = num_ix;
+      }
+      if (num[1] >= 0 && num[1] > res.max_val) {
+        res.max_val = num[1];
+        res.max_ix = num_ix;
+      }
+      return res;
+    }, { min_val: Infinity, min_ix: -1, max_val: -Infinity, max_ix: -1 });
 
   sum_part2 += ((number.min_ix % 9 + 1) * 10) + (number.max_ix % 9 + 1);
 });
