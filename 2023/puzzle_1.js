7ddb86dde4d9fb01f25bacaa355cb7f03512da2c
Valère Monseur <dev@vale.re>
2024-01-02 18:37:28 +0100

    make logic independent from array indexes

--- 2023/puzzle_1.js
+++ 2023/puzzle_1.js
@@ -10,29 +10,30 @@ console.log(`sum of all of the calibration values: ${sum_part1}`);
 
 // deno-fmt-ignore
 const numbers =
-  [ "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+  { "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+    "six": 6, "seven": 7, "eight": 8, "nine": 9 };
 
 let sum_part2 = 0;
 
 data.split("\n").forEach((line) => {
-  const number = numbers
+  const number = Object.keys(numbers)
     .map((num) => {
-      return [line.indexOf(num), line.lastIndexOf(num)];
+      return [line.indexOf(num), line.lastIndexOf(num), numbers[num]];
     })
-    .reduce((res, num, num_ix) => {
-      if (num[0] >= 0 && num[0] < res.min_val) {
-        res.min_val = num[0];
-        res.min_ix = num_ix;
+    .reduce((res, num) => {
+      if (num[0] >= 0 && num[0] < res.min_ix) {
+        res.min_ix = num[0];
+        res.min_val = num[2];
       }
-      if (num[1] >= 0 && num[1] > res.max_val) {
-        res.max_val = num[1];
-        res.max_ix = num_ix;
+      if (num[1] >= 0 && num[1] > res.max_ix) {
+        res.max_ix = num[1];
+        res.max_val = num[2];
       }
       return res;
-    }, { min_val: Infinity, min_ix: -1, max_val: -Infinity, max_ix: -1 });
+    }, { min_ix: Infinity, min_val: 0, max_ix: -Infinity, max_val: 0 });
 
-  sum_part2 += ((number.min_ix % 9 + 1) * 10) + (number.max_ix % 9 + 1);
+  sum_part2 += number.min_val * 10 + number.max_val;
 });
 
 console.log(`sum of all of the calibration values: ${sum_part2}`);
