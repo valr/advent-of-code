ccaf941ad4a793e50ab66bd8c79df10ff460c270
Valère Monseur <dev@vale.re>
2024-12-05 23:18:09 +0100

    aoc 2024 day 5

---
+++ 2024/day5/puzzle_5.go
@@ -0,0 +1,86 @@
+package main
+
+// go run 2024/day5/puzzle_5.go
+
+import (
+	_ "embed"
+	"fmt"
+	"slices"
+	"strings"
+
+	"advent-of-code/2023/util"
+
+	"github.com/samber/lo"
+)
+
+//go:embed input1.txt
+var input1 string
+
+//go:embed input2.txt
+var input2 string
+
+func main() {
+	l1, l2 := strings.Split(input1, "\n"), strings.Split(input2, "\n")
+	fmt.Println("solution 1:", solution1(l1, l2))
+	fmt.Println("solution 2:", solution2(l1, l2))
+}
+
+func solution1(s1, s2 []string) (result int) {
+	rule := make(map[int][]int)
+	for _, s := range s1 {
+		order := lo.Map(strings.Split(s, "|"), func(x string, i int) int {
+			return util.StrToInt(x)
+		})
+		if o, ok := rule[order[0]]; ok {
+			rule[order[0]] = append(o, order[1])
+		} else {
+			rule[order[0]] = []int{order[1]}
+		}
+	}
+next:
+	for _, s := range s2 {
+		page := lo.Map(strings.Split(s, ","), func(x string, i int) int {
+			return util.StrToInt(x)
+		})
+		for i, x := range page {
+			if len(lo.Intersect(page[:i], rule[x])) > 0 {
+				continue next
+			}
+		}
+		result += page[len(page)/2]
+	}
+	return result
+}
+
+func solution2(s1, s2 []string) (result int) {
+	rule := make(map[int][]int)
+	for _, s := range s1 {
+		order := lo.Map(strings.Split(s, "|"), func(x string, i int) int {
+			return util.StrToInt(x)
+		})
+		if o, ok := rule[order[0]]; ok {
+			rule[order[0]] = append(o, order[1])
+		} else {
+			rule[order[0]] = []int{order[1]}
+		}
+	}
+	for _, s := range s2 {
+		page := lo.Map(strings.Split(s, ","), func(x string, i int) int {
+			return util.StrToInt(x)
+		})
+		ordered := false
+		for i := 1; i < len(page); i++ {
+			intersect := lo.Intersect(page[:i], rule[page[i]])
+			if len(intersect) > 0 {
+				ordered = true
+				x1, x2 := slices.Index(page, page[i]), slices.Index(page, intersect[0])
+				page[x1], page[x2] = page[x2], page[x1]
+				i = 0 // restart loop at 1
+			}
+		}
+		if ordered {
+			result += page[len(page)/2]
+		}
+	}
+	return result
+}
