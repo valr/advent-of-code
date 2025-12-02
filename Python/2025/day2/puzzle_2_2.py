8d417faa45a1673a0c27adc85d7757e1d9bae4da
Valère Monseur <dev@vale.re>
2025-12-02 22:27:21 +0100

    put back original version of is_valid_id

--- Python/2025/day2/puzzle_2_2.py
+++ Python/2025/day2/puzzle_2_2.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 
+# # original version (poor performance)
+# def is_valid_id(id):
+#     s = str(id)
+#     sz = len(s)
+#     for n in range(1, sz // 2 + 1):
+#         l = [s[i : i + n] for i in range(0, sz, n)]
+#         if len(set(l)) <= 1:
+#             return True
+#     return False
+
+
 def is_valid_id(id: int) -> bool:
     # string periodicity test using doubling
     s = str(id)
